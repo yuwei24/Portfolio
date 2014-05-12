ff00005cd8f0f8ec02e42e170aef5b1fc25265d3
yuwei24 <yuwei24@gmail.com>
2014-05-12 13:39:40 +0800

    Revert "t"
    This reverts commit e741dfbecdf72cbd2405dca00da6a6b5ecb0ef80.

--- Portfolio.MVC/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Portfolio.MVC.MvcApplication" Language="C#" %>
--- _backup_20120627/Portfolio.MVC/Models/Portfolio.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'D:\Projects\Portfolio\Portfolio.MVC\Models\Portfolio.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
